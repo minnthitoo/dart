71b5dde2170275c0edc7aea4d82577a690b18cef
minnthitoo <mto.forlearning@gmail.com>
2023-07-24 14:08:11 +0630

    looping

---
+++ src/09.looping.dart
@@ -0,0 +1,14 @@
+void main(){
+  for(int i = 0; i < 11; i++){
+    print('Hello');
+  }
+  int x = 0;
+  while(x < 11){
+    print('Hello While');
+    x++;
+  }
+
+  do{
+    print('Hello do while');
+  }while(x < 11);
+}
